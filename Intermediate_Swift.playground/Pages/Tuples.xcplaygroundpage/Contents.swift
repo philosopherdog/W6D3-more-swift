807cc6955f9c251816b2bebc0497b05a18f7c115
Steven Thompson <steven.thompson.ca@gmail.com>
2016-04-17 11:09:47 -0400

    fixed up the tuples section

--- Intermediate_Swift.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
+++ Intermediate_Swift.playground/Pages/Tuples.xcplaygroundpage/Contents.swift
@@ -3,47 +3,60 @@
 import Foundation
 
 /*:
-“Tuples group multiple values into a single compound value. The values within a tuple can be of any type and do not have to be of the same type as each other.”
+### **Tuples**
+- Tuples are collection types
+- They group an arbitrary number of items of arbitrary types (like NSArray)
+- Like Arrays they are ordered, and accessible by index
+- They are also similar to dictionaries because they _can_ have keys/labels (But unlike dictionaries the labels are not proper data, like Strings)
+- Function parameters are actually tuples under the hood
+- They are handy for returning multiple values from functions (don't do this unless you know why you're doing it!)
+- Don't use tuples for complex data structures. Use Objects!
+- Question: How do you return more than 1 value from a method in objc?
 */
-let tuple1 = ("nuts", "figs", 15)
-
+/*: ##### _Example 1:_ */
+let tuple1 = ("nuts", "figs", 15) // Type => (String, String, Int)
 /*:
-You can access them using their index:
+Access them using their index:
 */
 tuple1.0
 tuple1.1
 tuple1.2
 /*:
-You can also give them labels
+ You can bind the elements of a tuple into separate constants or variables to access them:
 */
-let tuple2 = (lunch:"nuts", desert:"figs", cash:15)
-
+let (fats, fruit, _) = tuple1 // "_" ignores the value
+fats
+fruit
+/*: 
+ ##### _Example 2:_
+ - You can also give them labels:
+*/
+// make it let if you aren't mutating
+var tuple2 = (lunch:"nuts", desert:"figs", cash:15)
+/*: 
+Access using labels:
+*/
 tuple2.lunch
 tuple2.desert
 tuple2.cash
 
-/*:
-You can bind the elements of a tuple into separate constants or variables and then access them:
+tuple2.lunch = "Pizza" // setting/mutating
+tuple2.lunch // gets new value
+
+/*: 
+ ##### _Do:_
+ - Create a function that takes an array of 5 tuples, with age, height, and name (Int, Int, String).
+ - Write a for loop to the first 2 elements to 25, and 182 (centementers).
+ - If there is a match return the name. 
+ - If not return nil.
 */
 
-
-
-
-
-/*:
-underscore (_)
-“Alternatively, access the individual element values in a tuple using index numbers starting at zero:”
-
-Excerpt From: Apple Inc. “The Swift Programming Language (Swift 2.2).” iBooks. https://itun.es/ca/jEUH0.l
-“let http200Status = (statusCode: 200, description: "OK")”
-
-Excerpt From: Apple Inc. “The Swift Programming Language (Swift 2.2).” iBooks. https://itun.es/ca/jEUH0.l
-“print("The status code is \(http200Status.statusCode)")”
-
-Excerpt From: Apple Inc. “The Swift Programming Language (Swift 2.2).” iBooks. https://itun.es/ca/jEUH0.l
-
+/*: 
+ ##### _Do:_
+ 
+ - Create a function called fetchData that takes a _query_ parameter of _optional String_ type.
+ - Using _guard let_ return status 404 and nil if the query string is nil
+ - If the query string is not nil then return 200, and generate a random number between 0 and 1000 and return that as the string value
 */
-
-
 
 //: [Next](@next)
