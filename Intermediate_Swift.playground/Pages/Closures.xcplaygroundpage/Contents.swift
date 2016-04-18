fc5b3b567b740e880894f33be2d30eae7a684214
Steven Thompson <steven.thompson.ca@gmail.com>
2016-04-18 14:18:38 -0400

    Finishes up closure stuff. Must still add capture lists and unowned/weak

--- Intermediate_Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ Intermediate_Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -6,24 +6,31 @@ import Foundation
  ### **Named Closures (AKA Functions)**
  - "Closures are self-contained blocks of functionality that can be passed around and used in your code"
  - By this definition ordinary functions in Swift are closures!
- - Closures can also capture value from outside their scope (We'll come to this below)
+ - Closures can also capture value from outside their scope (More on this below)
  */
+
+/*:
+ ##### _Example of Function Parameter_
+ */
+
 func myFunc() {
     print("Function passed to another function")
 }
 
 let functionConstant = myFunc // passing a func to a variable/constant
-
-// func that takes a func paramater
+/*:
+func that takes a func paramater
+ */
 func funkyFunc(f:()->()) {
     f()
 }
 
 funkyFunc(myFunc)
+
 funkyFunc(functionConstant)
-
-// Using typealias for readability
-
+/*:
+Using typealias for readability
+*/
 typealias simpleFunc = ()->()
 
 func funkyFunc2(f:simpleFunc) {
@@ -33,8 +40,9 @@ func funkyFunc2(f:simpleFunc) {
 funkyFunc2(myFunc)
 
 /*:
- ##### Using Functions in CallBacks (cleaner than delegation!)
+ ##### Using Functions in CallBacks
  - Plain functions can be passed to another object to be used as a callback or completion handler
+ - Apple uses this in all modern parts of the SDK (blocks weren't added to objc until 2007 I think)
  */
 class MasterViewController {
     var detailViewController: DetailViewController?
@@ -43,7 +51,7 @@ class MasterViewController {
         print("Master is printing data sent from detail: \(data)")
     }
     
-    func eventFired() {
+    func fakeEventFired() {
         detailViewController = DetailViewController()
         detailViewController!.completionHandler = completionHandler
     }
@@ -53,69 +61,69 @@ class DetailViewController {
     typealias callBack = (data:String)->(Void)
     var completionHandler:callBack!
     
-    func buttonTapped() {
-        // do some stuff
+    func fakeButtonTap() {
+        // do some long running task
         sleep(2)
         // get some user input
-        let userInputFromTextField = "Pick up milk"
-        completionHandler(data:userInputFromTextField)
+        let fakeUserData = "Pick up milk"
+        completionHandler(data:fakeUserData)
     }
 }
 
 let masterViewController = MasterViewController()
-masterViewController.eventFired()
-masterViewController.detailViewController?.buttonTapped()
+masterViewController.fakeEventFired()
+masterViewController.detailViewController?.fakeButtonTap()
 
 /*:
  ##### _Do:_
+ - Question: When would it be better to use delegation over function/closure callbacks?
  - Download starter project
- - Write a function on MasterViewController that takes a String parameter
- - Set the passed in String to the label
- - In the DetailViewController make a property for that function
- - In prepareForSegue assign the function property to the function
- - Call the function from the button tap on detailViewController
+ - Write a function on MasterViewController that takes a String parameter (don't copy/paste please)
+ - MasterViewController's `updateWithData` method will take the passed in data and update the label
+ - In the DetailViewController make a property that allows you to pass Master's `updateWithData` function
+ - In prepareForSegue assign Master's `updateWithData` to the property on Detail
+ - Call the function from the button tap on Detail
  */
 
 /*:
  ##### **Unnamed Closures**
- - Closures are just like functions except they are unnamed
+ Closures are just like functions except they are unnamed
  */
-
-// closure so simple you can't call it!
-
+/*: closure so simple you can't call it! */
 //{
 //    print("the world's simplest closure")
 //}
 
+
 let close1 = { print("hello closure!") }
 
 close1() // call it
-
-// pass it to a function
-
-func fn1(close:()->()) {
+/*:
+pass it to a function
+*/
+func f1(close:()->()) {
     close()
 }
 
-fn1(close1)
-
-// closure with a paramater and no return value
-
-let close2 = {
-    (p: String) -> Void in
-    print(p)
-}
-
-// calling & passing in data
-close2("this closure takes a string argument")
+f1(close1)
 /*:
- - Notice: closures move their parameters & return types inside the block
- 
+closure with a paramater and no return value
+*/
+let close2 = {
+    (s: String) -> Void in
+    print(s)
+}
+/*:
+calling & passing in data:
+ */
+close2("How to call a closure with a string argument")
+/*:
+ Notice: closures move their parameters & return types inside the block
  `{ (parameters) -> returnType in statements }`
  */
-
-// closure with 2 parameters and a return
-
+/*:
+Closure with 2 parameters and a return:
+*/
 let multiply = {
     (num1: Int, num2: Int) -> Int in
     return num1 * num2
@@ -139,15 +147,17 @@ let result = multiply(12, 10)
  */
 
 let foods = ["zuccini", "banana", "avacado", "lettuce", "walnut", "tahini", "bread"]
-
-// doing it with a function
+/*:
+Doing it with a function:
+ */
 func sorter1(item1: String, item2: String) -> Bool {
     return item1 < item2
 }
+
 foods.sort(sorter1)
-
-// doing it with a closure
-
+/*:
+Doing it with a closure:
+*/
 foods.sort({
     (item1: String, item2: String) -> Bool in
     return item1 < item2
@@ -162,27 +172,30 @@ foods.sort({
 foods.sort({ item1, item2 in return item1 < item2 })
 
 /*:
- - Because this closure consists of a single expression, we can also omit the `return` keyword
+ Because this closure consists of a single expression, we can also omit the `return` keyword:
  */
 
 foods.sort({ item1, item2 in item1 < item2 })
 
 /*:
- - Swift automatically generates shorthand argument names for inline closures: $0, $1, $2
- - So we can omit the list of arguments and also the `in` keyword
+ Swift automatically generates shorthand argument names for inline closures: $0, $1, $2, etc.
+ 
+ So we can omit the list of arguments and also the `in` keyword:
  */
 
 foods.sort({ $0 < $1 })
 
 /*:
- - Swift's String type actually defines the `>` and `<` as a function that takes 2 strings and returns a Bool depending on their order. So we can even omit the generated shorthand argumens!
+ Swift's String type actually defines the `>` and `<` as a function that takes 2 strings and returns a Bool depending on their order. So we can even omit the generated shorthand arguments!
  */
 
 foods.sort(<)
 
 /*:
- - If a closure is the last argument of a function you can move it out of the function's round parentheses
- - Here's the long version of `sort(_:)` again with using trailing closure syntax
+ ###### _Trailing Closure Syntax_
+ If a closure is the last argument of a function you can move it out of the function's round parentheses
+ 
+ Here's the long version of `sort(_:)` again using trailing closure syntax
  */
 
 foods.sort(){
@@ -191,7 +204,7 @@ foods.sort(){
 }
 
 /*:
- - You can omit the round parentheses if it's the closure is the only argument
+ You can also omit the round parentheses if the closure is the only argument:
  */
 
 foods.sort{
@@ -203,7 +216,9 @@ foods.sort{
  ##### **Capturing Values**
  - Closures can capture constants/variables from their surrounding scope
  - Closures can use or even mutate these captured values even when they are passed to a function elsewhere
- - Swift allows nested functions and functions inside functions can use and mutate values from the surrounding scope
+ - Swift allows nested functions
+ - Nested functions can use and mutate values from the surrounding scope
+ - Closures inside functions have the same behaviour
  */
 
 func outerFunc() {
@@ -219,7 +234,7 @@ func outerFunc() {
 outerFunc()
 
 /*:
- - Instead of executing the inner function internally, let's return it.
+ Instead of executing the inner function internally, let's return it:
  */
 
 func outerFunc2()->(()->Int) {
@@ -231,33 +246,125 @@ func outerFunc2()->(()->Int) {
     return innerFunc
 }
 
-let myInnerFunc = outerFunc2()
+let myInnerFunc = outerFunc2() // returns the inner func
 myInnerFunc()
 myInnerFunc()
 
 /*:
- - In this last example, since we are not calling the function internally it makes no sense to name it
+ Since we are not calling the function internally it makes no sense to name it:
  */
 
 /*:
  ##### _Do:_
- - Rewrite the last function using a closure
+ Rewrite the last function using a closure:
+ */
+/*
+func outerFunc3()->(()->Int) {
+    
+}
  */
 
-func outerFunc3()->(()->Int) {
-    var num = 20
-    return {
-        num += 50
-        return num
+
+/*:
+ ##### **Higher Order Functions**
+ - Basically functions that take functions/closure and/or return them are called _Higher Order functions_
+ - Swift has some important build in Higher Order functions, besides `sort()`
+ - Let's look briefly at `map()`, `reduce()`, `filter()` (this is just an introduction)
+ */
+
+/*:
+ ##### `map()`
+ - Takes a closure as a parameter
+ - It simply calls the expression on each element and returns the resulting array
+*/
+
+let arr1 = [Int](1...10)
+let result2 = arr1.map({ (num:Int) -> String in
+    return "\(num)"
+})
+
+/*: 
+ ##### _Do:_
+ Simplify map using some of the techniques we talked about earlier:
+ */
+
+/*:
+ ##### `reduce()`
+ Similar to the `map()` but it reduces all of the elements to a single value
+ */
+// old way
+var result7 = 0
+for item in arr1 {
+    result7 += item
+}
+result7
+
+// reduce way
+let sum = arr1.reduce(0){ $0 + $1}
+sum
+
+/*: 
+ ###### _`filter()`_
+ Takes a closure and returns an array filtered according to whether it passes the test in the expression
+*/
+
+// forin way
+var result4: [Int] = []
+for item in arr1 {
+    if item%3 == 0 {
+        result4.append(item)
     }
 }
+result4
 
-let myInnerFunc2 = outerFunc3()
-myInnerFunc2()
-myInnerFunc2()
+// filter way
+let result3 = arr1.filter{ $0%3 == 0 }
+result3
+
+/*:
+ ##### _Do:_
+ - Let's practice a bit
+ - Below you will find an array that consists of a simple model object of type `Data`
+ - Please follow the instructions for each section
+*/
+
+struct Data {
+    let firstName: String
+    let lastName: String
+    let age: Int
+}
+
+let dataArray = [Data(firstName: "Jen", lastName: "Jones", age: 22),
+Data(firstName: "Jim", lastName: "Jones", age: 33),
+Data(firstName: "Jim", lastName: "Hendrix", age: 44),
+Data(firstName: "Jenny", lastName: "Craig", age: 20)]
+
+dataArray
+
+/*:
+ ##### _Do:_
+ - Using `map()` get an array of firstName lastName as a single string
+ */
 
 
 
+/*:
+ ##### _Do:_
+ - You can chain these functions together.
+ - Add sort so that it lists the names in ascending order:
+ */
+
+
+/*:
+ ##### _Do:_
+ - Using `reduce()` get the average age
+ */
+
+
+/*:
+ ##### _Do:_
+ - Using `filter()` get only those people whose name is "Jim"
+ */
 
 
 
