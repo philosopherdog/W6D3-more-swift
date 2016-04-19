5dfa23b5805862166df70d36c6c11cf2d7d2187c
Steven Thompson <steven.thompson.ca@gmail.com>
2016-04-18 21:41:01 -0400

    Final cleanup.

--- Intermediate_Swift.playground/Pages/PropertyObservers.xcplaygroundpage/Contents.swift
+++ Intermediate_Swift.playground/Pages/PropertyObservers.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 /*:
  ##### Property Observers
  - Allow you to intercept calls just before a property is set and just after
- - `willSet` `didSet`
+ - `willSet` and `didSet`
  - `willSet` is called just before a property is set, which allows you to grab the old value
  - `didSet` is called just after a property is set
  - The most common use case is when you want to update your interface once a property has been set, or when it gets refreshed
@@ -23,28 +23,32 @@ struct ToDo {
 
 var todoItem:[ToDo] = [] {
 didSet {
-//    sleep(2)
-    print(oldValue.last?.text)
+    // sleep(2)
+    print("didSet called", oldValue.last?.text)
 }
 willSet {
-//    sleep(2)
-    print(newValue.last?.text)
+    // sleep(2)
+    print("willSet called", newValue.last?.text)
 }
 }
 
 
 
-let todo = ToDo(text: "Pick up lettuce", priority: ToDo.Priority.High)
-todoItem.append(todo)
+let todo1 = ToDo(text: "Pick up lettuce", priority: ToDo.Priority.High)
+todoItem.append(todo1)
+let todo2 = ToDo(text: "Pick up nuts", priority: ToDo.Priority.Low)
+todoItem.append(todo2)
 
 /*:
  ##### _Do:_
  - Create a class called LibraryCard
- - Create a custom init that takes a random Int value and sets the LibraryCard instance's property _number_
- - Add a property observer to the _number_ property and intercept willSet
- - print the value before it is assigned
+ - Create a custom init that takes an Int parameter called _number_
+ - Assign a default value to _number_ property
+ - When you create a LibraryCard generate a random number to pass to the _number_ param.
+ - Add a property observer to the _number_ property and intercept _didSet_ and _willSet_
+ - Print the old/default value in willSet and the new value in didSet
  - Create an empty array that holds LibraryCards
- - Write a for in loop that creates 20 cards and adds them to the array
+ - Write a forin loop that creates 20 cards and adds them to the array
  */
 
 //: [Next](@next)
